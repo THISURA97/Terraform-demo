ea558a5c448d830143cb1e67e39fd07db7e8adcc
THISURA97 <tdinith481@gmail.com>
2021-12-28 17:48:54 +0530

    update m4

---
+++ global/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {
+  type = string
+  description = "AWS Access Key"
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type = string
+  description = "AWS Secret Key"
+  sensitive = true
+}
+
+variable "aws_region" {
+  type = string
+  description = "AWS Region for resources"
+  default = "us-east-1"
+}
